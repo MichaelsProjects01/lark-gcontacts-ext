50011ee9cf31d93e331c44355e231f27f16da765
MichaelsProjects01 <stathakismichael@gmail.com>
2026-05-31 12:04:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  build: { outDir: 'dist' },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        callback: resolve(__dirname, 'callback.html'),
+      },
+    },
+  },
 });
